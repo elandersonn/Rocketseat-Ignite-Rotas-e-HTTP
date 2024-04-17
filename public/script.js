d7d56ed1df7a53f990ae366d608f79435e9f4ba7
Elanderson Cavalcanti Gomes <elanderson.cg@gmail.com>
2024-04-17 01:36:31 -0300

    Add Captcha

--- public/script.js
+++ public/script.js
@@ -1,47 +1,74 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const loginForm = document.getElementById('login-form');
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
 
-  loginForm.addEventListener('submit', function(event) {
-      event.preventDefault();
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
+    const captchaInput = document.getElementById("captcha").value;
+    const captchaText = document.getElementById("captcha-text").textContent;
+    const captchaCheck = checkCaptcha(captchaInput, captchaText);
+    if (captchaCheck == false) {
+      return
+    }
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const validationResult = validatePassword(password);
+    if (validationResult == false) {
+      return
+    }
 
-      const validationResult = validatePassword(password);
+    const userData = {
+      username: username,
+      password: password,
+    };
 
-      if (validationResult !== true) {
-        alert("A senha não atende aos critérios especificados: " + validationResult);
-        return;
-      }
-
-      const userData = {
-          username: username,
-          password: password
-      };
-
-      fetch('http://localhost:3333/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(userData)
-      })
-      .then(response => response.json())
-      .then(data => {
-        alert('Usuário e senha válidos. Cadastrados corretamente!')
-        document.getElementById('password').value = '';
-        document.getElementById('username').value = '';
+    fetch("http://localhost:3333/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Usuário e senha válidos. Cadastrados corretamente!");
+        document.getElementById("password").value = "";
+        document.getElementById("username").value = "";
       })
       .catch((error) => {
-          console.error(error);
+        console.error(error);
       });
   });
 });
 
+function checkCaptcha(captchaInput, captchaText) {
+  fetch("http://localhost:3333/check-captcha", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ captchaInput, captchaText }),
+    })
+      .then((response) => response.json())
+      .then((serverResponse) => {
+        if (!serverResponse) {
+          alert("Código CAPTCHA incorreto. Tente novamente.");
+          generateNewCaptcha();
+          return;
+        }
+        document.getElementById("captcha").value = "";
+        generateNewCaptcha();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+}
 
 function validatePassword(password) {
   if (password.length < 8) {
-    return "A senha deve ter pelo menos 8 caracteres.";
+    alert("A senha deve conter pelo menos 8 caracteres.")
+    return false
   }
 
   const hasUpperCase = /[A-Z]/.test(password);
@@ -50,17 +77,31 @@ function validatePassword(password) {
   const hasSymbols = /[^A-Za-z0-9]/.test(password);
 
   if (!hasUpperCase) {
-    return "A senha deve conter pelo menos uma letra maiúscula.";
+    alert("A senha deve conter pelo menos uma letra maiúscula.")
+    return false
   }
   if (!hasLowerCase) {
-    return "A senha deve conter pelo menos uma letra minúscula.";
+    alert("A senha deve conter pelo menos uma letra minúscula.")
+    return false
   }
   if (!hasNumbers) {
-    return "A senha deve conter pelo menos um número.";
+    alert("A senha deve conter pelo menos um número.")
+    return false
   }
   if (!hasSymbols) {
-    return "A senha deve conter pelo menos um símbolo especial.";
+    alert("A senha deve conter pelo menos um símbolo especial.")
+    return false
   }
 
-  return true;
+  return true
+}
+
+function generateNewCaptcha() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let captchaText = "";
+  for (let i = 0; i < 5; i++) {
+    captchaText += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  document.getElementById("captcha").value = ""; 
+  document.getElementById("captcha-text").textContent = captchaText;
 }
--- src/server.ts
+++ src/server.ts
@@ -5,23 +5,22 @@ import crypto from 'node:crypto';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const app = fastify();
-const port = 3333;
+const app = fastify()
+const port = 3333
+
+const hash = crypto.createHash('sha256')
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 app.register(fastifyStatic, {
   root: path.join(__dirname, '..', 'public'),
   prefix: '/',
 });
 
-app.post('/login', async (req, res) => {
+app.post('/user', async (req, res) => {
   const { username, password }: any = req.body;
 
-  const hash = crypto.createHash('sha256');
   hash.update(password);
   const encryptedPassword = hash.digest('hex');
-
   const userData = await knex('users')
     .insert({
       id: crypto.randomUUID(),
@@ -34,6 +33,28 @@ app.post('/login', async (req, res) => {
   return userData
 });
 
+app.get('/user', async (req, res) => {
+  const {username, password}: any = req.body
+  if (!username || !password) {
+    return res.code(400).send({ message: 'Missing username or password' });
+  }
+
+  const hash = crypto.createHash('sha256');
+  hash.update(password);
+  const encryptedPassword = hash.digest('hex');
+
+  const user = await knex('users')
+    .where({ username }) 
+    .andWhere('encrypted_password', encryptedPassword) 
+    .first(); 
+
+  if (!user) {
+    return res.code(404).send({ message: 'User not found' });
+  }
+
+  return res.code(200)
+})
+
 app.get('/users', async () => {
   const users = await knex('users').select('*');
   return users
@@ -42,6 +63,13 @@ app.get('/users', async () => {
 app.delete('/users', async () => {
   const users = await knex('users').delete('*')
   return users
+})
+
+app.post('/check-captcha', async (req, res) => {
+  const {captchaText, captchaInput}: any = req.body
+  const isValid = captchaInput ? captchaInput === captchaText : false
+
+  return isValid
 })
 
 app.listen({ port })
